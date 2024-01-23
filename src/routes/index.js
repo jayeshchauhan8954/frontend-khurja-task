3af87af4240079b2439b75a33b2dc0aaccadda0a
jayesh <jayesh.singh@staqo.com>
2024-01-23 14:28:03 +0530

    deleted

--- src/routes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SignupPage } from '../pages/SignupPage/SignupPage';
-export { default as LoginPage } from '../pages/LoginPage/LoginPage'
-export { default as HomePage } from '../pages/HomePage/HomePage'
