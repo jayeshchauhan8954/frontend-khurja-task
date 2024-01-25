733606a1bf5ee6bf343fa84bb6f9402b40ce364b
vipin28448 <tv28448@gmail.com>
2024-01-25 14:15:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { LoginPage, SignupPage, HomePage } from './routes'
+import { LoginPage, SignupPage, HomePage, Slidebar } from './routes'
 import routes from './routes/routes'
 import { AuthProvider } from './contexts/authContext'
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
           <Route path={routes._auth.signup} element={<SignupPage />} />
           <Route path={routes._auth.login} element={<LoginPage />} />
           <Route path={routes._auth.home} element={<HomePage/>}/>
+          <Route path={routes._auth.slidebar} element={<Slidebar/>}/>
         </Routes>
       </Router>
     </AuthProvider>
