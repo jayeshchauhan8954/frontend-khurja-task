6df54d647dba70df7a5440fdccd1df0dbbdf6243
hritikch02 <hritikchaudhary2002@gmail.com>
2024-01-25 13:01:13 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,8 @@ import routes from './routes/routes'
 import { AuthProvider } from './contexts/authContext'
 const App = () => {
   return (
-<<<<<<< HEAD
-    <UserProvider >
-      <BrowserRouter>
-=======
     <AuthProvider>
       <Router>
->>>>>>> 9245d378fd1d51feab220f2349deb9d0806b1013
         <Routes>
           <Route path={routes._auth.signup} element={<SignupPage />} />
           <Route path={routes._auth.login} element={<LoginPage />} />
