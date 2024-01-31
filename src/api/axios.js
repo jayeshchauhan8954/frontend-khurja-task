49371d23b53de467d37e0659a2a66b5d6eb61eec
rekharaghav1234 <rekharaghav90120@gmail.com>
2024-01-31 11:46:05 +0530

    task page

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,3 +6,8 @@ export const postData = async (url, payload) => {
     const response = await axios.post(`${BASE_URL}${url}`, payload);
     return response;
 }
+
+export const getData = async (url) => {
+    const response = await axios.get(`${BASE_URL}${url}`);
+    return response;
+}
--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import dummyData from '../../utils/task_dummy_data'
+import Box from '@mui/material/Box'
+import { getData } from '../../api/axios'
 const TaskPage = () => {
+  const [task, setTask] = useState(null);
+  const getTask = async () => {
+    try {
+      let response = await getData('/intern/api/v1/get-all-tasks')
+      console.log(response)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  useEffect(() => {
+    getTask()
+  }, [task])
   return (
-    <div>
-      {dummyData.map((task)=>(
-        <ul key={task._id}>
-            <li>{task._id}</li>
-            <li>{task.task_name}</li>
-            <li>{task.task_status}</li>
-        </ul>
+    <div >
+
+      {dummyData.map((task) => (
+        <Box>
+          <table style={{ float: "left", marginLeft: '10px' }} >
+            <th>
+              <td>Id</td>
+              <td>Task Name</td>
+              <td>Status</td>
+              <td>Updated By</td>
+            </th>
+            <ul key={task.user}>
+
+              <li style={{ listStyle: 'none' }}>{task._id}</li>
+              <li style={{ listStyle: 'none' }}>{task.task_name}</li>
+              <li style={{ listStyle: 'none' }}>{task.task_status}</li>
+              <li style={{ listStyle: 'none' }}>{task.updated_by}</li>
+
+            </ul>
+          </table>
+        </Box>
       ))}
     </div>
   )
