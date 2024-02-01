6c6cef84dffb44585989cc0c992a8c8301a853ec
vipin28448 <tv28448@gmail.com>
2024-02-01 11:32:31 +0530

    updates from dev-jayesh branch

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,11 +6,8 @@ export const postData = async (url, payload) => {
     const response = await axios.post(`${BASE_URL}${url}`, payload);
     return response;
 }
-<<<<<<< HEAD
-=======
 
 export const getData = async (url, headers) => {
     const response = await axios.get(url, { headers });
     return response;
 };
->>>>>>> 69625ab54b0eb46fca5193f093618386c8a6ad8e
